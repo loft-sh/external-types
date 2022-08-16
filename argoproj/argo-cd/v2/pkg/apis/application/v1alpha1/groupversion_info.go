331f729d9e24cf6d74503d4b5469a1ed752f9e29
Carl Montanari <carl.r.montanari@gmail.com>
2022-08-16 14:49:09 -0700

    fix: groupname for argoproj

--- argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/groupversion_info.go
+++ argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/groupversion_info.go
@@ -1,6 +1,6 @@
-// Package v1alpha1 contains API Schema definitions for the config v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the application v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=storage.loft.sh
+// +groupName=application.argoproj.io
 package v1alpha1
 
 import (
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "application.argoproj.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "argoproj.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
