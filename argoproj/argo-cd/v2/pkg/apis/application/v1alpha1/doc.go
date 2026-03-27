7eaea27b75f28afa06d8f888167e41faf58e59af
Florian MEDJA <florian.medja@vcluster.com>
2026-03-27 15:53:11 -0400

    feat: Add // +k8s:openapi-gen=true to doc.go for support external type generation (#8)

--- argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/doc.go
+++ argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/doc.go
@@ -4,5 +4,6 @@
 
 // +k8s:deepcopy-gen=package,register
 // +k8s:defaulter-gen=TypeMeta
+// +k8s:openapi-gen=true
 // +groupName=application.argoproj.io
 package v1alpha1 // import "github.com/loft-sh/external-types/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
