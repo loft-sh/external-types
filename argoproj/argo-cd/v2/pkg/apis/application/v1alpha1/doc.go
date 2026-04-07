f471c7c023f23fa08bee62f26078ce579af91c4c
Florian Medja <florian.medja@vcluster.com>
2026-04-07 08:13:02 -0400

    fix: force openapi to generate a dot-separated model name (like it does for k8s.io packages) instead of a slash-separated one.

--- argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/doc.go
+++ argoproj/argo-cd/v2/pkg/apis/application/v1alpha1/doc.go
@@ -5,5 +5,6 @@
 // +k8s:deepcopy-gen=package,register
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
+// +k8s:openapi-model-package=github.com.loft-sh.external-types.argoproj.argo-cd.v2.pkg.apis.application.v1alpha1
 // +groupName=application.argoproj.io
 package v1alpha1 // import "github.com/loft-sh/external-types/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
